a28d77258d03bc63b4b034842ad58d3c596d6891
Robert Schmidt <robert.schmidt@openairinterface.org>
2024-05-21 14:17:35 +0200

    Reset num_ul_pdcch after scheduling
    Co-authored-by: hsum <ming-hong.hsu@eurecom.fr>

--- openair1/PHY/NR_TRANSPORT/nr_dci.c
+++ openair1/PHY/NR_TRANSPORT/nr_dci.c
@@ -243,6 +243,7 @@ void nr_generate_dci_top(processingData_L1tx_t *msgTx,
 
   for (int i=0; i<msgTx->num_ul_pdcch; i++)
     nr_generate_dci(msgTx->gNB,&msgTx->ul_pdcch_pdu[i].pdcch_pdu.pdcch_pdu_rel15,txdataF,amp,frame_parms,slot);
+  msgTx->num_ul_pdcch = 0;
   for (int i=0; i<msgTx->num_dl_pdcch; i++)
     nr_generate_dci(msgTx->gNB,&msgTx->pdcch_pdu[i].pdcch_pdu_rel15,txdataF,amp,frame_parms,slot);
 
