turkeryilmaz/openairinterface5g

feat(nrLDPC_coding_interface): Use reworked interface in PHY simulators

openair1/SIMULATION/NR_PHY/dlschsim.c
@@ -35,6 +35,7 @@
 #include "PHY/defs_nr_common.h"
 #include "PHY/defs_nr_UE.h"
 #include "PHY/types.h"
+#include "PHY/CODING/nrLDPC_coding/nrLDPC_coding_interface.h"
 #include "PHY/INIT/nr_phy_init.h"
 #include "PHY/MODULATION/modulation_eNB.h"
 #include "PHY/MODULATION/modulation_UE.h"
@@ -380,7 +381,7 @@ int main(int argc, char **argv)
 	RC.gNB[0] = calloc(1, sizeof(PHY_VARS_gNB));
 	gNB = RC.gNB[0];
 	initNamedTpool(gNBthreads, &gNB->threadPool, true, "gNB-tpool");
-        initFloatingCoresTpool(dlsch_threads, &nrUE_params.Tpool, false, "UE-tpool");
+  initFloatingCoresTpool(dlsch_threads, &nrUE_params.Tpool, false, "UE-tpool");
 	//gNB_config = &gNB->gNB_config;
 	frame_parms = &gNB->frame_parms; //to be initialized I suppose (maybe not necessary for PBCH)
 	frame_parms->nb_antennas_tx = n_tx;
@@ -430,6 +431,9 @@ int main(int argc, char **argv)
 
 	//nr_init_frame_parms_ue(&UE->frame_parms);
 	//init_nr_ue_transport(UE, 0);
+
+  UE->nrLDPC_coding_interface_flag = gNB->nrLDPC_coding_interface_flag;
+
   NR_UE_DLSCH_t dlsch_ue[NR_MAX_NB_LAYERS > 4? 2:1] = {0};
   int num_codeword = NR_MAX_NB_LAYERS > 4? 2:1;
   nr_ue_dlsch_init(dlsch_ue, num_codeword, 5);
@@ -513,7 +517,12 @@ int main(int argc, char **argv)
         unsigned char output[rel15->rbSize * NR_SYMBOLS_PER_SLOT * NR_NB_SC_PER_RB * 8 * NR_MAX_NB_LAYERS] __attribute__((aligned(32)));
         bzero(output,rel15->rbSize * NR_SYMBOLS_PER_SLOT * NR_NB_SC_PER_RB * 8 * NR_MAX_NB_LAYERS);
 	if (input_fd == NULL) {
-	  nr_dlsch_encoding(gNB, frame, slot, &dlsch->harq_process, frame_parms,output,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+    if (gNB->nrLDPC_coding_interface_flag) {
+      msgDataTx.num_pdsch_slot = 1;
+	    nr_dlsch_encoding_slot(gNB, &msgDataTx, frame, slot, frame_parms, output, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+    } else {
+	    nr_dlsch_encoding(gNB, frame, slot, &dlsch->harq_process, frame_parms, output, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+    }
 	}
 
 	for (SNR = snr0; SNR < snr1; SNR += snr_step) {
@@ -569,20 +578,35 @@ int main(int argc, char **argv)
       }
       uint32_t dlsch_bytes = a_segments*1056;  // allocated bytes per segment
       __attribute__ ((aligned(32))) uint8_t b[dlsch_bytes];
-      ret = nr_dlsch_decoding(UE,
-                              &proc,
-                              0,
-                              channel_output_fixed,
-                              &UE->frame_parms,
-                              dlsch0_ue,
-                              harq_process,
-                              frame,
-                              nb_symb_sch,
-                              slot,
-                              harq_pid,
-                              dlsch_bytes,
-                              b,
-                              available_bits);
+      if (gNB->nrLDPC_coding_interface_flag) {
+        uint8_t DLSCH_ids[1] = {0};
+        short *p_channel_output_fixed = channel_output_fixed;
+        uint8_t *p_b = b;
+        int available_bits_array[1] = { available_bits };
+        ret = nr_dlsch_decoding_slot(UE,
+                                     &proc,
+                                     dlsch0_ue,
+                                     &p_channel_output_fixed,
+                                     &p_b,
+                                     available_bits_array,
+                                     1,
+                                     DLSCH_ids);
+      } else {
+        ret = nr_dlsch_decoding(UE,
+                                &proc,
+                                0,
+                                channel_output_fixed,
+                                &UE->frame_parms,
+                                dlsch0_ue,
+                                harq_process,
+                                frame,
+                                nb_symb_sch,
+                                slot,
+                                harq_pid,
+                                dlsch_bytes,
+                                b,
+                                available_bits);
+      }
 
       vcd_signal_dumper_dump_function_by_name(VCD_SIGNAL_DUMPER_FUNCTIONS_DLSCH_DECODING0, VCD_FUNCTION_OUT);
 

openair1/SIMULATION/NR_PHY/ulschsim.c
@@ -33,6 +33,7 @@
 #include "PHY/defs_nr_common.h"
 #include "PHY/defs_nr_UE.h"
 #include "PHY/defs_gNB.h"
+#include "PHY/CODING/nrLDPC_coding/nrLDPC_coding_interface.h"
 #include "PHY/INIT/nr_phy_init.h"
 #include "PHY/NR_REFSIG/refsig_defs_ue.h"
 #include "PHY/MODULATION/modulation_eNB.h"
@@ -448,6 +449,12 @@ int main(int argc, char **argv)
 
   nr_init_ul_harq_processes(UE->ul_harq_processes, NR_MAX_ULSCH_HARQ_PROCESSES, UE->frame_parms.N_RB_UL, UE->frame_parms.nb_antennas_tx);
 
+  UE->nrLDPC_coding_interface_flag = gNB->nrLDPC_coding_interface_flag;
+
+  if (UE->nrLDPC_coding_interface_flag) {
+    initFloatingCoresTpool(1, &nrUE_params.Tpool, false, "UE-tpool");
+  }
+
   unsigned char harq_pid = 0;
   unsigned int TBS = 8424;
   unsigned int available_bits;
@@ -530,7 +537,12 @@ int main(int argc, char **argv)
   unsigned int G = available_bits;
 
   if (input_fd == NULL) {
-    nr_ulsch_encoding(UE, ulsch_ue, frame_parms, harq_pid, TBS>>3, G);
+    if (UE->nrLDPC_coding_interface_flag) {
+      uint8_t ULSCH_ids[] = {0};
+      nr_ulsch_encoding_slot(UE, ulsch_ue, 0, 0, &G, 1, ULSCH_ids);
+    } else {
+      nr_ulsch_encoding(UE, ulsch_ue, frame_parms, harq_pid, TBS>>3, G);
+    }
   }
   
   printf("\n");
@@ -594,18 +606,26 @@ int main(int argc, char **argv)
       exit(-1);
 #endif
 
-     int nbDecode = nr_ulsch_decoding(gNB, UE_id, channel_output_fixed, frame_parms, rel15_ul, frame, subframe, harq_pid, G);
-     int nb_ok = 0;
-     if (nbDecode > 0)
-       while (nbDecode > 0) {
-         notifiedFIFO_elt_t *req = pullTpool(&gNB->respDecode, &gNB->threadPool);
-         ret = nr_postDecode_sim(gNB, req, &nb_ok);
-         delNotifiedFIFO_elt(req);
-         nbDecode--;
-       }
+      if (gNB->nrLDPC_coding_interface_flag) {
+        nr_ulsch_decoding_slot(gNB, frame_parms, frame, subframe, &G, &UE_id, 1);
+        bool crc_valid = check_crc(harq_process_gNB->b, lenWithCrc(1, (harq_process_gNB->TBS) << 3), crcType(1, (harq_process_gNB->TBS) << 3));
+        if (!crc_valid) {
+          n_errors++;
+        }
+      } else {
+        int nbDecode = nr_ulsch_decoding(gNB, UE_id, channel_output_fixed, frame_parms, rel15_ul, frame, subframe, harq_pid, G);
+        int nb_ok = 0;
+        if (nbDecode > 0)
+          while (nbDecode > 0) {
+            notifiedFIFO_elt_t *req = pullTpool(&gNB->respDecode, &gNB->threadPool);
+            ret = nr_postDecode_sim(gNB, req, &nb_ok);
+            delNotifiedFIFO_elt(req);
+            nbDecode--;
+          }
 
-      if (ret)
-        n_errors++;
+        if (ret)
+          n_errors++;
+      }
     }
     
     printf("*****************************************\n");

saffanazyan07/oai_w-agf

openair1/SIMULATION/NR_PHY/dlschsim.c
@@ -35,6 +35,7 @@
 #include "PHY/defs_nr_common.h"
 #include "PHY/defs_nr_UE.h"
 #include "PHY/types.h"
+#include "PHY/CODING/nrLDPC_coding/nrLDPC_coding_interface.h"
 #include "PHY/INIT/nr_phy_init.h"
 #include "PHY/MODULATION/modulation_eNB.h"
 #include "PHY/MODULATION/modulation_UE.h"
@@ -380,7 +381,7 @@ int main(int argc, char **argv)
 	RC.gNB[0] = calloc(1, sizeof(PHY_VARS_gNB));
 	gNB = RC.gNB[0];
 	initNamedTpool(gNBthreads, &gNB->threadPool, true, "gNB-tpool");
-        initFloatingCoresTpool(dlsch_threads, &nrUE_params.Tpool, false, "UE-tpool");
+  initFloatingCoresTpool(dlsch_threads, &nrUE_params.Tpool, false, "UE-tpool");
 	//gNB_config = &gNB->gNB_config;
 	frame_parms = &gNB->frame_parms; //to be initialized I suppose (maybe not necessary for PBCH)
 	frame_parms->nb_antennas_tx = n_tx;
@@ -430,6 +431,7 @@ int main(int argc, char **argv)
 
 	//nr_init_frame_parms_ue(&UE->frame_parms);
 	//init_nr_ue_transport(UE, 0);
+
   NR_UE_DLSCH_t dlsch_ue[NR_MAX_NB_LAYERS > 4? 2:1] = {0};
   int num_codeword = NR_MAX_NB_LAYERS > 4? 2:1;
   nr_ue_dlsch_init(dlsch_ue, num_codeword, 5);
@@ -513,7 +515,8 @@ int main(int argc, char **argv)
         unsigned char output[rel15->rbSize * NR_SYMBOLS_PER_SLOT * NR_NB_SC_PER_RB * 8 * NR_MAX_NB_LAYERS] __attribute__((aligned(32)));
         bzero(output,rel15->rbSize * NR_SYMBOLS_PER_SLOT * NR_NB_SC_PER_RB * 8 * NR_MAX_NB_LAYERS);
 	if (input_fd == NULL) {
-	  nr_dlsch_encoding(gNB, frame, slot, &dlsch->harq_process, frame_parms,output,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+    msgDataTx.num_pdsch_slot = 1;
+	  nr_dlsch_encoding(gNB, &msgDataTx, frame, slot, frame_parms, output, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 	}
 
 	for (SNR = snr0; SNR < snr1; SNR += snr_step) {
@@ -569,20 +572,18 @@ int main(int argc, char **argv)
       }
       uint32_t dlsch_bytes = a_segments*1056;  // allocated bytes per segment
       __attribute__ ((aligned(32))) uint8_t b[dlsch_bytes];
+      uint8_t DLSCH_ids[1] = {0};
+      short *p_channel_output_fixed = channel_output_fixed;
+      uint8_t *p_b = b;
+      int available_bits_array[1] = { available_bits };
       ret = nr_dlsch_decoding(UE,
                               &proc,
-                              0,
-                              channel_output_fixed,
-                              &UE->frame_parms,
                               dlsch0_ue,
-                              harq_process,
-                              frame,
-                              nb_symb_sch,
-                              slot,
-                              harq_pid,
-                              dlsch_bytes,
-                              b,
-                              available_bits);
+                              &p_channel_output_fixed,
+                              &p_b,
+                              available_bits_array,
+                              1,
+                              DLSCH_ids);
 
       vcd_signal_dumper_dump_function_by_name(VCD_SIGNAL_DUMPER_FUNCTIONS_DLSCH_DECODING0, VCD_FUNCTION_OUT);
 

openair1/SIMULATION/NR_PHY/ulschsim.c
@@ -33,6 +33,7 @@
 #include "PHY/defs_nr_common.h"
 #include "PHY/defs_nr_UE.h"
 #include "PHY/defs_gNB.h"
+#include "PHY/CODING/nrLDPC_coding/nrLDPC_coding_interface.h"
 #include "PHY/INIT/nr_phy_init.h"
 #include "PHY/NR_REFSIG/refsig_defs_ue.h"
 #include "PHY/MODULATION/modulation_eNB.h"
@@ -91,27 +92,6 @@ void deref_sched_response(int _)
   exit(1);
 }
 
-int nr_postDecode_sim(PHY_VARS_gNB *gNB, notifiedFIFO_elt_t *req, int *nb_ok)
-{
-  ldpcDecode_t *rdata = (ldpcDecode_t*) NotifiedFifoData(req);
-  NR_UL_gNB_HARQ_t *ulsch_harq = rdata->ulsch_harq;
-  int r = rdata->segment_r;
-
-  bool decodeSuccess = (rdata->decodeIterations <= rdata->decoderParms.numMaxIter);
-  ulsch_harq->processedSegments++;
-
-  if (decodeSuccess) {
-    memcpy(ulsch_harq->b+rdata->offset,
-           ulsch_harq->c[r],
-           rdata->Kr_bytes - (ulsch_harq->F>>3) -((ulsch_harq->C>1)?3:0));
-  }
-
-  // if all segments are done
-  if (rdata->nbSegments == ulsch_harq->processedSegments)
-    return *nb_ok == rdata->nbSegments;
-  return 0;
-}
-
 nrUE_params_t nrUE_params;
 
 nrUE_params_t *get_nrUE_params(void) {
@@ -143,7 +123,6 @@ int main(int argc, char **argv)
   NR_DL_FRAME_PARMS *frame_parms;
   double sigma;
   unsigned char qbits = 8;
-  int ret=0;
   int loglvl = OAILOG_WARNING;
   uint64_t SSB_positions=0x01;
   uint16_t nb_symb_sch = 12;
@@ -448,6 +427,8 @@ int main(int argc, char **argv)
 
   nr_init_ul_harq_processes(UE->ul_harq_processes, NR_MAX_ULSCH_HARQ_PROCESSES, UE->frame_parms.N_RB_UL, UE->frame_parms.nb_antennas_tx);
 
+  initFloatingCoresTpool(1, &nrUE_params.Tpool, false, "UE-tpool");
+
   unsigned char harq_pid = 0;
   unsigned int TBS = 8424;
   unsigned int available_bits;
@@ -530,7 +511,8 @@ int main(int argc, char **argv)
   unsigned int G = available_bits;
 
   if (input_fd == NULL) {
-    nr_ulsch_encoding(UE, ulsch_ue, frame_parms, harq_pid, TBS>>3, G);
+    uint8_t ULSCH_ids[] = {0};
+    nr_ulsch_encoding(UE, ulsch_ue, 0, 0, &G, 1, ULSCH_ids);
   }
   
   printf("\n");
@@ -594,18 +576,11 @@ int main(int argc, char **argv)
       exit(-1);
 #endif
 
-     int nbDecode = nr_ulsch_decoding(gNB, UE_id, channel_output_fixed, frame_parms, rel15_ul, frame, subframe, harq_pid, G);
-     int nb_ok = 0;
-     if (nbDecode > 0)
-       while (nbDecode > 0) {
-         notifiedFIFO_elt_t *req = pullTpool(&gNB->respDecode, &gNB->threadPool);
-         ret = nr_postDecode_sim(gNB, req, &nb_ok);
-         delNotifiedFIFO_elt(req);
-         nbDecode--;
-       }
-
-      if (ret)
+      nr_ulsch_decoding(gNB, frame_parms, frame, subframe, &G, &UE_id, 1);
+      bool crc_valid = check_crc(harq_process_gNB->b, lenWithCrc(1, (harq_process_gNB->TBS) << 3), crcType(1, (harq_process_gNB->TBS) << 3));
+      if (!crc_valid) {
         n_errors++;
+      }
     }
     
     printf("*****************************************\n");